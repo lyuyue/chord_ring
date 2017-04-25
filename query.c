3245bfd9395305d74f99360f447b95a29bc94cec
Yue Lyu <lyu.yue1992@gmail.com>
2017-04-25 13:35:54 -0400

    fix bug

--- query.c
+++ query.c
@@ -125,7 +125,7 @@ void get_closest_preceding_finger(struct CTX *ctx, struct Node *node, uint32_t i
 void closest_preceding_finger_handler(struct CTX *ctx, struct Node *result, uint32_t id) {
     printf("closest_preceding_finger_handler ID: %u\n", id);
     if (id < ctx->local_id) id += power(2, MAXM);
-    for (int i = MAXM; i > 0; i--) {
+    for (int i = MAXM - 1; i >= 0; i--) {
         uint32_t finger_id = ctx->finger[i].node.id;
         if (finger_id < ctx->local_id) finger_id += power(2, MAXM);
         if (finger_id > ctx->local_id && ctx->finger[i].node.id < id) {
