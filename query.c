2d7d29c9ad6ab8c5f6f0531965396a2300b85e12
Yue Lyu <lyu.yue1992@gmail.com>
2017-04-25 21:42:02 -0400

    test

--- query.c
+++ query.c
@@ -139,6 +139,8 @@ void closest_preceding_finger_handler(struct CTX *ctx, struct Node *result, uint
 
     if (id < ctx->local_id) id += power(2, MAXM);
     for (int i = MAXM - 1; i >= 0; i--) {
+        if (ctx->finger[i].node.addr.sin_addr.s_addr == 0) continue;
+        
         uint32_t finger_id = ctx->finger[i].node.id;
         if (finger_id < ctx->local_id) finger_id += power(2, MAXM);
 
